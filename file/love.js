445860b0aacefd26fb0b269334772d01e1ace11f
WangRay <983604371@qq.com>
2025-08-02 21:27:26 +0800

    xiaxue

--- file/love.js
+++ file/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Hi  Yangying ! Click on this heart ", 23, 10);
+            ctx.fillText("Hi  XiaXue ! Click on this heart ", 23, 10);
             ctx.restore();
         },
         clear: function() {
